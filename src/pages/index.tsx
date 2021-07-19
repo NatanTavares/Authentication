3046811fe9a38a142d43785455e6367aa83028e5
NatanTavares <nattantavares.s15@gmail.com>
2021-07-19 18:55:08 -0300

    feat(form): Implements the login form

---
+++ src/pages/index.tsx
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import { FormEvent, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const { signIn } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const credentials = { email, password };
+    await signIn(credentials);
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Auth</title>
+      </Head>
+
+      <form className={styles.container} onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="email">
+          <p>Email:</p>
+
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="password">
+          <p>Password:</p>
+
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="submit" className={styles.button}>
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+}
