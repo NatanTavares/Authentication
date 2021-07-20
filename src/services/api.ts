324016d3491bc482e5cc29dd1d518e77441582b7
NatanTavares <nattantavares.s15@gmail.com>
2021-07-20 17:20:30 -0300

    feat(axios): Implements the request queue in axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue: any[] = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -16,31 +18,62 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       if (error.response.data?.code === "token.expired") {
         cookies = parseCookies();
-        const { "@auth.refreshToken": refreshToken } = cookies;
 
-        api
-          .post("/refresh", {
-            refreshToken,
-          })
-          .then((response) => {
-            const { token } = response.data;
-            setCookie(undefined, "@auth.token", token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: "/",
-            });
+        const originalConfig = error.config;
 
-            setCookie(
-              undefined,
-              "@auth.refreshToken",
-              response.data.refreshToken,
-              {
+        if (!isRefreshing) {
+          isRefreshing = true;
+          const { "@auth.refreshToken": refreshToken } = cookies;
+
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
+              setCookie(undefined, "@auth.token", token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: "/",
-              }
-            );
+              });
 
-            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+              setCookie(
+                undefined,
+                "@auth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                }
+              );
+
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.failure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
           });
+        });
       } else {
         // deslogar o usuário
       }
